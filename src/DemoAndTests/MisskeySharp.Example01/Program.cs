ba7ab64d102dda43d4ba1fae743ffc9b18ecc532
a32kita <a32kita@yahoo.co.jp>
2023-06-24 21:02:24 +0900

    wip

--- src/DemoAndTests/MisskeySharp.Example01/Program.cs
+++ src/DemoAndTests/MisskeySharp.Example01/Program.cs
@@ -1,4 +1,5 @@
 ﻿using MisskeySharp.Entities;
+using System.Collections.Specialized;
 using System.Net;
 
 namespace MisskeySharp.Example01
@@ -41,7 +42,6 @@ namespace MisskeySharp.Example01
                     goto auth;
                 }
                 Console.WriteLine("認証に成功しました;");
-                Console.WriteLine("Access Token: {0}", misskey.AccessToken);
                 Console.WriteLine("Access Token: {0}", s_maskString(misskey.AccessToken));
                 Console.WriteLine();
             }
@@ -51,7 +51,7 @@ namespace MisskeySharp.Example01
             }
 
             // デモ投稿
-            var demoText = "(Debug) API リクエスト テスト\nこれは Misskey API のコール試験投稿です。\n" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff");
+            var demoText = "(Debug) API リクエスト テスト\nこれは Misskey API のコール試験投稿です。\n" + DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff") + "\n\n#a32kita_debug_notes";
             Console.WriteLine("次の内容を投稿してもよろしいですか？");
             Console.WriteLine(demoText);
             Console.WriteLine();
@@ -59,19 +59,48 @@ namespace MisskeySharp.Example01
             Console.ReadLine();
             try
             {
-                var resp = await misskey.PostAsync<Note, NoteCreated>("notes/create", new Note()
-                {
-                    Text = demoText,
-                    Visibility = "followers",
-                });
+                //var resp = await misskey.PostAsync<Note, NoteCreated>("notes/create", new Note()
+                //{
+                //    Text = demoText,
+                //    //Visibility = "followers",
+                //});
 
-                Console.WriteLine("ノートの投稿が完了しました: {0}", resp.CreatedNote.Id);
+                //Console.WriteLine("ノートの投稿が完了しました: {0}", resp.CreatedNote.Id);
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Error: {ex.GetType().Name}");
                 Console.WriteLine($"       {ex.Message}");
             }
+            Console.WriteLine();
+
+
+            // デモ検索
+            var demoKeyword = "#a32kita_debug_notes";
+            Console.WriteLine("次のキーワードでノートを検索します: {0}", demoKeyword);
+            Console.WriteLine("[Enter] キーを押下して続行します ...");
+            Console.ReadLine();
+            try
+            {
+                var resp = await misskey.PostAsync<NoteSearchQuery, NoteCollection>("notes/search", new NoteSearchQuery()
+                {
+                    Query = demoKeyword,
+                    Limit = 20,
+                });
+
+                Console.WriteLine("ノート検索結果;");
+                foreach (var note in resp)
+                {
+                    Console.WriteLine(" {0} | {1}", note.User.Username.PadRight(15), note.Text.Replace("\n", " "));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.GetType().Name}");
+                Console.WriteLine($"       {ex.Message}");
+            }
+            Console.WriteLine();
+
 
             // デモ終わり
             Console.WriteLine("サンプル プログラムによるデモが完了しました。");
---
+++ src/MisskeySharp/Entities/NoteSearchQuery.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace MisskeySharp.Entities
+{
+    public class NoteSearchQuery : MisskeyApiEntitiesBase
+    {
+        public string Query
+        {
+            get;
+            set;
+        }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string SinceId
+        {
+            get;
+            set;
+        }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string UntilId
+        {
+            get;
+            set;
+        }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public int Limit
+        {
+            get;
+            set;
+        }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public int Offset
+        {
+            get;
+            set;
+        }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string Host
+        {
+            get;
+            set;
+        }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string UserId
+        {
+            get;
+            set;
+        }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public string ChannelId
+        {
+            get;
+            set;
+        }
+    }
+}
