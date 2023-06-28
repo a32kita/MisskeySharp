8952645427142046a6e98fc5fc0d3002a0e1fdfe
a32kita <a32kita@yahoo.co.jp>
2023-06-28 19:39:57 +0900

    wip

---
+++ src/MisskeySharp/TestScenario001/UnitTest1.cs
@@ -0,0 +1,17 @@
+using MisskeySharp;
+
+namespace TestScenario001
+{
+    [TestClass]
+    public class UsersTest
+    {
+        [TestMethod]
+        public void TestMethod101()
+        {
+            var username = "a32kita";
+            var host = "https://misskey.io";
+
+
+        }
+    }
+}
